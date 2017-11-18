2c11462b7d954df56c83d26d880438c1f5d635e1
Neel <neelsankaran@gmail.com>
2017-11-18 14:38:40 -0800

    server stuff is now threaded

--- src/org/usfirst/frc/team6038/robot/Server.java
+++ src/org/usfirst/frc/team6038/robot/Server.java
@@ -4,32 +4,24 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.concurrent.ArrayBlockingQueue;
 import org.usfirst.frc.team6038.framework.Database;
 
 public class Server 
 {
-
-	public static boolean isRobotRunning = true;
+	private PrintStream p;
+	private Socket ss;
+	private ServerSocket server;
+	private ArrayBlockingQueue<String> tempdata;
 	
-	public Server() throws IOException
+	public Server(int port) throws IOException
 	{
-		ServerSocket server = new ServerSocket(6968);
+		server = new ServerSocket(port);
 		System.out.println("Server socket established");
-		Socket ss = server.accept();
+		ss = server.accept();
 		System.out.println("Server accepting");
-		PrintStream p = new PrintStream(ss.getOutputStream());
+		p = new PrintStream(ss.getOutputStream(), false);
 		System.out.println("Server Running... ");
-
-		double values[] = getData();
-		
-		while (isRobotRunning)
-		{
-				// Right encoder Left encoder Front Right power Front Left power Throttle Steering wheel
-				p.print(values[0] + values[1] + values[2] + values[3] + values[4] + values[5] + values[6]);
-				p.println();
-		}
-		server.close();
 	}
 
 	private double[] getData()
@@ -46,8 +38,16 @@ public class Server
 				};
 	}
 	
-	public void turnOff()
+	public void iterate() throws IOException{
+
+		double values[] = getData();
+		// Right encoder Left encoder Front Right power Front Left power Throttle Steering wheel
+		p.print(values[0] + " " + values[1] + " " + values[2] + " " + values[3] + " " + values[4] + " " + values[5] + " " + values[6] + "/n");
+	}
+	
+	public void close() throws IOException
 	{
-		isRobotRunning = false;
+		p.flush();
+		server.close();
 	}
 }
