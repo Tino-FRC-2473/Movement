a423176512cf63f89ca8dc2f23d210c9df9dcefc
Neel <neelsankaran@gmail.com>
2017-11-18 15:38:44 -0800

    flusher updater thread added

--- src/org/usfirst/frc/team6038/robot/Server.java
+++ src/org/usfirst/frc/team6038/robot/Server.java
@@ -13,6 +13,7 @@ public class Server
 	private Socket ss;
 	private ServerSocket server;
 	private ArrayBlockingQueue<String> tempdata;
+	private String[] tempArray;
 	
 	public Server(int port) throws IOException
 	{
@@ -21,29 +22,27 @@ public class Server
 		ss = server.accept();
 		System.out.println("Server accepting");
 		p = new PrintStream(ss.getOutputStream(), false);
+		tempdata = new ArrayBlockingQueue<String>(6);
 		System.out.println("Server Running... ");
+		FlusherThread flusher = new FlusherThread(p);
+		flusher.start();
+		UpdaterThread updater = new UpdaterThread();
+		updater.start();
 	}
 
-	private double[] getData()
-	{
-		return new double[]
-				{
-						Database.getInstance().getNumeric(RobotMap.FRONT_RIGHT_ENC), 
-						Database.getInstance().getNumeric(RobotMap.FRONT_LEFT_ENC),
-						Database.getInstance().getNumeric("pfr"),
-						Database.getInstance().getNumeric("pfl"),
-						Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z),
-						Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X),
-						Database.getInstance().getNumeric(RobotMap.GYRO_YAW)
-				};
-	}
-	
-	public void iterate() throws IOException{
-
-		double values[] = getData();
-		// Right encoder Left encoder Front Right power Front Left power Throttle Steering wheel
-		p.print(values[0] + " " + values[1] + " " + values[2] + " " + values[3] + " " + values[4] + " " + values[5] + " " + values[6] + "/n");
-	}
+//	private double[] getData()
+//	{
+//		return new double[]
+//				{
+//						Database.getInstance().getNumeric(RobotMap.FRONT_RIGHT_ENC), 
+//						Database.getInstance().getNumeric(RobotMap.FRONT_LEFT_ENC),
+//						Database.getInstance().getNumeric("pfr"),
+//						Database.getInstance().getNumeric("pfl"),
+//						Database.getInstance().getNumeric(ControlsMap.THROTTLE_Z),
+//						Database.getInstance().getNumeric(ControlsMap.STEERING_WHEEL_X),
+//						Database.getInstance().getNumeric(RobotMap.GYRO_YAW)
+//				};
+//	}
 	
 	public void close() throws IOException
 	{
